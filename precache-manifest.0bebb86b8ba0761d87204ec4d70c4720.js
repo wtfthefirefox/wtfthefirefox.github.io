6520230924e1b66da65299a791d742babfc0071b
thefirefox <wtfthefirefox@gmail.com>
2020-09-14 00:56:34 +0300

    first commit

---
+++ precache-manifest.0bebb86b8ba0761d87204ec4d70c4720.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c02b42458351c1c1af71094e38467c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3816060e716733006f34",
+    "url": "/static/css/main.e36a2fbe.chunk.css"
+  },
+  {
+    "revision": "72318b401324fb96dcfc",
+    "url": "/static/js/2.2280a850.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2280a850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3816060e716733006f34",
+    "url": "/static/js/main.7d54459f.chunk.js"
+  },
+  {
+    "revision": "188a9c4e6fa7a0f84fe9",
+    "url": "/static/js/runtime-main.911d0a6e.js"
+  },
+  {
+    "revision": "9e14b4e72dec1db9a60d2636bbfe64f2",
+    "url": "/static/media/SFNSDisplay-Regular.9e14b4e7.ttf"
+  },
+  {
+    "revision": "ee8a0f193c944cccbcfe99cd468103ab",
+    "url": "/static/media/SFNSText-Medium.ee8a0f19.ttf"
+  },
+  {
+    "revision": "0538ddc92082d2ef2e7b375519825dac",
+    "url": "/static/media/SFProDisplay-Semibold.0538ddc9.ttf"
+  },
+  {
+    "revision": "192744e661b293fd535158a9932ea353",
+    "url": "/static/media/SFProDisplay-Semibold.192744e6.eot"
+  },
+  {
+    "revision": "2c151db57c2192e8e7a26ea88d40fa46",
+    "url": "/static/media/SFProDisplay-Semibold.2c151db5.woff"
+  },
+  {
+    "revision": "af64ab08547cc18daa32439d1f6c9c05",
+    "url": "/static/media/SFProDisplay-Semibold.af64ab08.woff2"
+  },
+  {
+    "revision": "6ba4b187e963c08d4e9d88ddaa78a401",
+    "url": "/static/media/TTCommonsRegular.6ba4b187.otf"
+  },
+  {
+    "revision": "ff83e3c28ac7e917e003e11062683205",
+    "url": "/static/media/arrow-back.ff83e3c2.svg"
+  },
+  {
+    "revision": "cd9afc51788ac21490bb36b41f099108",
+    "url": "/static/media/arrow-next.cd9afc51.svg"
+  },
+  {
+    "revision": "08403e2a72fc03e2b4acfa6bf6b03fae",
+    "url": "/static/media/date.08403e2a.svg"
+  },
+  {
+    "revision": "45c8c5dfc30521692de3c20d9b958506",
+    "url": "/static/media/img.45c8c5df.svg"
+  },
+  {
+    "revision": "675177355b86f65f79d7bd1e6a0527c6",
+    "url": "/static/media/target.67517735.svg"
+  }
+]);
